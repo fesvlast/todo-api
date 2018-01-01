78ceb9fca3eaeb1df808d00e449293891bfbef3d
Tymofii <fesvlast@gmail.com>
2018-01-01 11:59:07 +0200

    Add tests for POST /users/login route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -230,7 +230,7 @@ describe('POST /users', () => {
           expect(user).toBeDefined();
           expect(user.password).not.toBe(password);
           done();
-        });
+        }).catch( (e)=>done(e));
       });
   });
 
@@ -254,5 +254,58 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', ()=>{
+  it('should login user and return auth token', (done)=>{
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toBeDefined();
+      }).end((err, res) =>{
+        if(err){
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user)=>{
+          expect(user.tokens[0].access).toContain('auth');
+          expect(user.tokens[0].token).toContain(res.headers['x-auth']);
+          done();
+        }).catch((e)=>{
+          done(e);
+        });
+      });
+  });
+
+  it('should reject invalid login', (done) =>{
+
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password +'1'
+      })
+      .expect(400)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).not.toBeDefined();
+      })
+      .end((err, res)=>{
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user)=>{
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e)=>{
+          done(e);
+        });
+
+      });
   });
 });
